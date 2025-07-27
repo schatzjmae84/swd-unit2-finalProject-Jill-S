7bad5714df774d618b9a7e7e9198249f76cf3fd3
schatzjmae84 <schatzjm84@gmail.com>
2025-07-27 17:17:26 -0500

    "Added the @CrossOrigin annotation to my ActivityController to allow cross-origin requests from any origin. Added a "name" column to my Activity table in mySQL."

--- back-end-springBoot/pup-places/src/main/java/com/my_project/pup_places/controllers/ActivityController.java
+++ back-end-springBoot/pup-places/src/main/java/com/my_project/pup_places/controllers/ActivityController.java
@@ -6,13 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 
+@CrossOrigin(origins = "*", maxAge = 3600) // Allow cross-origin requests from any origin
 @RestController
 @RequestMapping("/api/idealInfo") // Base URL for the API
 public class ActivityController {
